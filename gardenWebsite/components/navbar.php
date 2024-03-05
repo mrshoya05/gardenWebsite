fb49c15e608f21b3cbc33aa5f283ec448a1d954b
shoyab khan <129213723+mrshoya05@users.noreply.github.com>
2024-03-05 07:54:19 -0800

    Add files via upload

---
+++ gardenWebsite/components/navbar.php
@@ -0,0 +1,23 @@
+<?<php>
+<nav class="navbar navbar-expand-lg navbar-light bg-custom">
+        <div class="container-fluid">
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav custom-nav d-flex justify-content-center">
+                    <a class="nav-link" aria-current="page" href="/index.html">Home</a>
+                    <a class="nav-link" href="/aboutus.html">About</a>
+                    <a class="nav-link" href="/contact.html">Contact</a>
+                    <a class="nav-link" href="/gallary.html">Gallery</a>
+                    <a class="nav-link" href="/Events.html">Events</a>
+                    <a class="nav-link" href="#">Feedback</a>
+
+                </div>
+            </div>
+        </div>
+    </nav>
+
+</php>
